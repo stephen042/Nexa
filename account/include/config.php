7c09357f17f001f7cbbc10ccddba77c7b9b86595
stephen042 <makuochukwu042@gmail.com>
2024-08-12 12:11:44 +0100

    upload

--- account/include/config.php
+++ account/include/config.php
@@ -7,19 +7,20 @@ define("WEB_EMAIL", "support@nexarockfinance.com"); // Your Website Email
 $web_url = WEB_URL;
 
 function dbConnect(){
-    // $servername = "localhost";
-    // $username = "nexarpar_corp"; //DATABASE USERNAME
-    // $password = "m7sTA6VSNiRMKju"; //DATABASE PASSWORD
-    // $database = "nexarpar_corp"; //DATABASE NAME
-    // $port = "3306"; //DATABASE port NB default is 3300
+    $servername = "localhost";
+    $username = "nexarpar_corp"; //DATABASE USERNAME
+    $password = "m7sTA6VSNiRMKju"; //DATABASE PASSWORD
+    $database = "nexarpar_corp"; //DATABASE NAME
+    $port = "3306"; //DATABASE port NB default is 3300
+    $dns = "mysql:host=$servername;port=$port;dbname=$database";
 
     // local details 
-    $servername = "localhost";
-    $username = "root"; //DATABASE USERNAME
-    $password = ""; //DATABASE PASSWORD
-    $database = "nexarock"; //DATABASE NAME
-    $port = "3308"; //DATABASE port NB default is 3300
-    $dns = "mysql:host=$servername;port=$port;dbname=$database";
+    // $servername = "localhost";
+    // $username = "root"; //DATABASE USERNAME
+    // $password = ""; //DATABASE PASSWORD
+    // $database = "nexarock"; //DATABASE NAME
+    // $port = "3308"; //DATABASE port NB default is 3300
+    // $dns = "mysql:host=$servername;port=$port;dbname=$database";
 
     try {
         $conn = new PDO($dns, $username, $password);
